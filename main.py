6d83dced687e67833131911d978cfe3382f4277b
AlecMMiller <alecmichaelmiller@gmail.com>
2017-09-23 21:17:37 -0700

    Formatting fixes

--- main.py
+++ main.py
@@ -2,30 +2,33 @@ from Tkinter import *
 
 iteratorVal = 0
 
+
 def iterator():
     global iteratorVal
     iteratorVal += 1
     return iteratorVal - 1
 
-def updateScreen():
+
+def update_screen():
     acceleration_output.update()
+
 
 class InputStructure:
     def __init__(self, label, units, root, master, offset):
         self.root = root
-        self.inVar=StringVar()
+        self.inVar = StringVar()
         self.panel = master
         vcmd = (root.register(self.validate),
                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
-        self.label = Label(self.panel,text=label).grid(row=offset,column=0)
-        self.inputFld = Entry(self.panel, textvariable=self.inVar, validate = 'key', validatecommand = vcmd)
-        self.inputFld.grid(row=offset,column=1)
-        self.label = Label(self.panel,text=units).grid(row=offset,column=2)
+        self.label = Label(self.panel, text=label).grid(row=offset, column=0)
+        self.inputFld = Entry(self.panel, textvariable=self.inVar, validate='key', validatecommand=vcmd)
+        self.inputFld.grid(row=offset, column=1)
+        self.label = Label(self.panel, text=units).grid(row=offset, column=2)
 
     def validate(self, action, index, value_if_allowed,
                        prior_value, text, validation_type, trigger_type, widget_name):
         if text in '0123456789.-':
-            if (value_if_allowed == "" or value_if_allowed == "-"):
+            if value_if_allowed == "" or value_if_allowed == "-":
                 return True
             try:
                 float(value_if_allowed)
@@ -35,24 +38,25 @@ class InputStructure:
         else:
             return False
 
-    def getInput(self):
+    def get_input(self):
         try:
             return float(self.inputFld.get())
         except ValueError:
             return 0
 
+
 class OutputStructure:
     def __init__(self, label, units, root, master, offset):
         self.offset = offset
         self.root = root
-        self.inVar=StringVar()
+        self.inVar = StringVar()
         self.panel = master
         self.outputVar = StringVar()
         self.outputVar.set("ERR")
-        Label(self.panel,text=label).grid(row=offset,column=0)
-        self.output_label = Label(self.panel,textvariable=self.outputVar)
-        self.output_label.grid(row=offset,column=1)
-        Label(self.panel,text=units).grid(row=offset,column=2)
+        Label(self.panel, text=label).grid(row=offset, column=0)
+        self.output_label = Label(self.panel, textvariable=self.outputVar)
+        self.output_label.grid(row=offset, column=1)
+        Label(self.panel, text=units).grid(row=offset, column=2)
 
     def math(self):
         print "Error, unset function"
@@ -65,7 +69,6 @@ class OutputStructure:
         except ZeroDivisionError:
             self.outputVar.set('NaN')
 
-    
 
 root1 = Tk()
 panel = Frame(root1).grid()
@@ -76,21 +79,23 @@ LabelLabel = Label(text='Field').grid(row=iteratorVal,column=0)
 InputLabel = Label(text='Value').grid(row=iteratorVal,column=1)
 UnitsLabel = Label(text='Units').grid(row=iterator(),column=2)
 
-mass_input = InputStructure('Mass','kg',root1,panel,iterator())
-force_input = InputStructure('Force','N',root1,panel,iterator())
+mass_input = InputStructure('Mass', 'kg', root1, panel, iterator())
+force_input = InputStructure('Force', 'N', root1, panel, iterator())
 
-BreakLabel = Label(text='---------').grid(row=iterator(),column=1)
+BreakLabel = Label(text='---------').grid(row=iterator(), column=1)
 
-acceleration_output = OutputStructure('Acceleration','m/s^2',root1,panel,4)
+acceleration_output = OutputStructure('Acceleration', 'm/s^2', root1, panel, 4)
 
-def accelerationCalculator(self):
-    acceleration = force_input.getInput() / mass_input.getInput()
+
+def calculate_acceleration(self):
+    acceleration = force_input.get_input() / mass_input.get_input()
     return acceleration
 
-acceleration_output.math = accelerationCalculator.__get__(acceleration_output, OutputStructure)
 
-mbutton = Button(panel,text='Update',command=updateScreen).grid(columnspan=3)
+acceleration_output.math = calculate_acceleration.__get__(acceleration_output, OutputStructure)
 
-updateScreen()
+mbutton = Button(panel, text='Update', command=update_screen).grid(columnspan=3)
+
+update_screen()
 
 root1.mainloop()
