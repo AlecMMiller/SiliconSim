c887a6f4cbeea5de77e4e40e737b1d13da8f398d
AlecMMiller <alecmichaelmiller@gmail.com>
2017-09-26 18:39:01 -0700

    Changed classes to no longer do math themselves, added fields that will be used in the actual project rather than demo fields

--- gui_elements.py
+++ gui_elements.py
@@ -1,17 +1,24 @@
 from Tkinter import *
+from decimal import Decimal
 
 
 class InputStructure:
-    def __init__(self, label, units, root, master, offset):
+
+    def __init__(self, label, units, default, root, master, offset):
         self.root = root
         self.inVar = StringVar()
+        self.hybridVar = 0
+        self.hybridLabelVar = StringVar()
+        self.inVar.set(default)
         self.panel = master
         vcmd = (root.register(self.validate),
                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
-        self.label = Label(self.panel, text=label).grid(row=offset, column=0)
+        Label(self.panel, text=label).grid(row=offset, column=0)
         self.inputFld = Entry(self.panel, textvariable=self.inVar, validate='key', validatecommand=vcmd)
         self.inputFld.grid(row=offset, column=1)
-        self.label = Label(self.panel, text=units).grid(row=offset, column=2)
+        Label(self.panel, text=units).grid(row=offset, column=3)
+        self.hybridLabel = Label(self.panel, textvariable=self.hybridLabelVar)
+        self.hybridLabel.grid(row=offset, column=2)
 
     def validate(self, action, index, value_if_allowed,
                        prior_value, text, validation_type, trigger_type, widget_name):
@@ -32,29 +39,43 @@ class InputStructure:
         except ValueError:
             return 0
 
+    def set_hybrid(self, hybrid_val):
+        self.hybridLabelVar.set("{:.2E}".format(Decimal(hybrid_val)))
+        self.hybridVar = hybrid_val
+
+    def get_hybrid(self):
+        return self.hybridVar
+
 
 class OutputStructure:
-    registry = []
 
     def __init__(self, label, units, root, master, offset):
-        self.registry.append(self)
         self.offset = offset
         self.root = root
         self.panel = master
-        self.outputVar = StringVar()
-        self.outputVar.set("ERR")
+        self.hybridVar = 0
+        self.hybridLabelVar = StringVar()
+        self.hybridLabelVar.set("ERR")
+        self.referenceVar = 0
+        self.referenceLabelVar = StringVar()
+        self.referenceLabelVar.set("ERR")
         Label(self.panel, text=label).grid(row=offset, column=0)
-        self.output_label = Label(self.panel, textvariable=self.outputVar)
+        self.output_label = Label(self.panel, textvariable=self.referenceLabelVar)
         self.output_label.grid(row=offset, column=1)
-        Label(self.panel, text=units).grid(row=offset, column=2)
+        self.hybrid_label = Label(self.panel, textvariable=self.hybridLabelVar)
+        self.hybrid_label.grid(row=offset, column=2)
+        Label(self.panel, text=units).grid(row=offset, column=3)
 
-    def math(self):
-        print "Error, unset function"
-        return 0
+    def set_reference(self, reference_val):
+        self.referenceLabelVar.set(reference_val)
+        self.referenceVar = reference_val
 
-    def update(self):
-        try:
-            value = self.math()
-            self.outputVar.set(value)
-        except ZeroDivisionError:
-            self.outputVar.set('NaN')
+    def get_hybrid(self):
+        return self.hybridVar
+
+    def set_hybrid(self, hybrid_val):
+        self.hybridLabelVar.set(hybrid_val)
+        self.hybridVar = hybrid_val
+
+    def get_hybrid(self):
+        return self.hybridVar
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from Tkinter import *
 from gui_elements import InputStructure, OutputStructure
 
+mobility = 0.06
+kSilicon = 11.8
+kOxide = 3.9
+vScaleIndex = 0.5
+datum = 0.1
+breakdownCo = 8*10**46
+breakdownIndex = -33.5
+depLayer = 20
+trackMultiplier = 50
+
 iteratorVal = 0
+print "started"
 
 
 def iterator():
     global iteratorVal
     iteratorVal += 1
     return iteratorVal - 1
-
-
-def update_screen():
-    for output in OutputStructure.registry:
-        output.update()
 
 
 root1 = Tk()
@@ -22,22 +28,54 @@ root1.title('Parameters')
 
 LabelLabel = Label(text='Field').grid(row=iteratorVal, column=0)
 InputLabel = Label(text='Value').grid(row=iteratorVal, column=1)
-UnitsLabel = Label(text='Units').grid(row=iterator(), column=2)
+UnitsLabel = Label(text='Hybrid').grid(row=iteratorVal, column=2)
+UnitsLabel = Label(text='Units').grid(row=iterator(), column=3)
 
-mass_input = InputStructure('Mass', 'kg', root1, panel, iterator())
-force_input = InputStructure('Force', 'N', root1, panel, iterator())
+k_val = InputStructure('K Value', '', 0.7, root1, panel, iterator())
+cdl = InputStructure('CD = L', 'nm', 65, root1, panel, iterator())
+ox_thick = InputStructure('Gate ox thickness', 'nm', 1.5, root1, panel, iterator())
+poly_sheet = InputStructure('Poly Sheet R', 'ohms/sq', 25, root1, panel, iterator())
+sd_sheet = InputStructure('S-D Sheet R', 'ohms/sq', 40, root1, panel, iterator())
+metal_sheet = InputStructure('Metal Sheet R', 'ohms/sq', 0.02, root1, panel, iterator())
+supply_voltage = InputStructure('Supply Voltage', 'V', 1.5, root1, panel, iterator())
+ref_power = InputStructure('Ref Power', 'uW', 1, root1, panel, iterator())
 
 BreakLabel = Label(text='---------').grid(row=iterator(), column=1)
 
-acceleration_output = OutputStructure('Acceleration', 'm/s^2', root1, panel, 4)
+gate_capacitance = OutputStructure('Gate Capacitance', 'fF', root1, panel, iterator())
+gate_leakage = OutputStructure('Gate Leakage Current', 'mA/mm^2', root1, panel, iterator())
+transistor_gain = OutputStructure('Transistor Gain K', 'mA/V^2', root1, panel, iterator())
+poly_rc = OutputStructure('Poly RC Const', 'ps', root1, panel, iterator())
+metal_rc = OutputStructure('Metal RC Const', 'ps', root1, panel, iterator())
+thresh = OutputStructure('Threshold Voltage', 'V', root1, panel, iterator())
+max_idsat = OutputStructure('Max Idsat', 'mA', root1, panel, iterator())
+depletion_width = OutputStructure('Depletion Layer Width', 'mm', root1, panel, iterator())
+channel_doping_m = OutputStructure('Channel Doping', '1/m^3', root1, panel, iterator())
+channel_doping_cm = OutputStructure('Channel Doping', '1/cm^3', root1, panel, iterator())
+max_lateral = OutputStructure('Max Lateral Field', 'MV/m', root1, panel, iterator())
+max_oxide = OutputStructure('Max Oxide Field', 'MV/m', root1, panel, iterator())
+sd_transit = OutputStructure('S-D Transit Time', 'ps', root1, panel, iterator())
+on_resistance = OutputStructure('ON Resistance', 'kohm', root1, panel, iterator())
+well_breakdown = OutputStructure('Well Breakdown Voltage', 'V', root1, panel, iterator())
+max_current = OutputStructure('Max Current/Micron', 'mA/micron', root1, panel, iterator())
+power_density = OutputStructure('Power Density', 'MW/m^2', root1, panel, iterator())
 
 
-def calculate_acceleration(self):
-    acceleration = force_input.get_input() / mass_input.get_input()
-    return acceleration
+def update_screen():
+    try:
+        kVal = k_val.get_input()
+        gVal = kVal ** vScaleIndex
+        k_val.set_hybrid(gVal)
+        cdl.set_hybrid(cdl.get_input()*kVal)
+        ox_thick.set_hybrid(ox_thick.get_input()*kVal)
+        poly_sheet.set_hybrid(poly_sheet.get_input()/kVal)
+        sd_sheet.set_hybrid(sd_sheet.get_input()/kVal)
+        metal_sheet.set_hybrid(metal_sheet.get_input()/kVal)
+        supply_voltage.set_hybrid(supply_voltage.get_input()*kVal)
+        ref_power.set_hybrid(ref_power.get_input()*gVal**3/kVal)
+    except ZeroDivisionError:
+        print "zero division occurred"
 
-
-acceleration_output.math = calculate_acceleration.__get__(acceleration_output, OutputStructure)
 
 mbutton = Button(panel, text='Update', command=update_screen).grid(columnspan=3)
 
